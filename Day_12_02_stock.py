380e5483bf1da2ebd6b37e9d71f2e52c7a94cac2
yunhui21 <yunhui21@gmail.com>
2020-11-17 12:56:56 +0900

    stock_data preprocessing _second

--- Day_12_02_stock.py
+++ Day_12_02_stock.py
@@ -13,11 +13,13 @@ np.set_printoptions(linewidth=1000)
 # (예측 결과는 그래프로 표시합니다)
 
 
-def make_data(seq_len):
+def make_data_1(seq_len):
     # 문제
     # x와 y 데이터를 만드세요.
     # stock = pd.read_csv('data/stock_daily.csv') # #처리된 줄을 읽어 들이지 못한다.
     stock = np.loadtxt('data/stock_daily.csv', delimiter=',') # numpy에서는 #을 보면 주석으로 받아들인다.
+    stock = stock[::-1]
+    stock = preprocessing.scale(stock) # 원래 가격으로 가기 위해서는 돌려야 한다. scale은 변수 표준편차하고 ...해서
     print(stock)
 
     rng = [i for i in range(len(stock) - seq_len )] # +1 삭제(8번째가 y라서)
@@ -32,20 +34,61 @@ def make_data(seq_len):
     y = np.float32(y)
     print(x.shape, y.shape) # (725, 7, 5) (725, 1)
 
-    return model_selection.train_test_split(x, y, train_size=0.7)
+    return model_selection.train_test_split(x, y, train_size=0.7, shuffle=False) # 시계열데이터는 shuffle 안된다.
     # regression
 
-def rnn_stock_1():
+# 문제
+# 야휴 주식 사이트에서 구글 데이터를 다룬로드 받아서 이전에 만든 모델에 적용하세요.
+
+def make_data_2(seq_len):
+    stock = pd.read_csv('data/GOOG.csv')
+    # 1번
+    # stock = [stock.Open, stock.High, stock.Low, stock.Volume, stock.Close] # 필요한 컬럼재배치 판다스의 기능
+    # stock = np.transpose(stock)
+    # print(stock.shape) #(252, 5)
+
+    # --------------------------------------------------------------------- #
+    # 2번
+    stock = stock.values
+    # stock = np.transpose([stock[:, 1], stock[:, 2], stock[:, 3], stock[:, 6], stock[:, 4]])
+    stock = np.hstack([stock[:, 1:4], stock[:, -1:], stock[:, 4:5]])
+    stock = np.float32(stock)
+    print(stock.shape, stock.dtype) # (252, 5) float32
+    # --------------------------------------------------------------------- #
+
+    # stock = stock[::-1]
+    stock = preprocessing.scale(stock) # 원래 가격으로 가기 위해서는 돌려야 한다. scale은 변수 표준편차하고 ...해서
+    # print(stock)
+
+    rng = [i for i in range(len(stock) - seq_len )] # +1 삭제(8번째가 y라서)
+
+    x = [stock[s:s+seq_len] for s in rng]
+    # y = [stock[s+seq_len][-1] for s in rng] # 마지막 거래가만 필요하다.
+    y = [stock[s+seq_len, -1:] for s in rng] # 마지막 거래가만 필요하다. 2차원의 값을 가져야 한다.
+
+    print(y[:3]) # [809.559998, 808.380005, 806.969971]
+
+    x = np.float32(x)
+    y = np.float32(y)
+    print(x.shape, y.shape) # (725, 7, 5) (725, 1)
+
+    return model_selection.train_test_split(x, y, train_size=0.7, shuffle=False) # 시계열데이터는 shuffle 안된다.
+    # regression
+
+
+def rnn_stock(fn_make):
     seq_len, n_features = 7, 5
-    x_train, x_test, y_train, y_test = make_data(seq_len)
+    x_train, x_test, y_train, y_test = fn_make(seq_len)
 
     # regresson mini_batch_size 적용하지 않는다.
 
     ph_x = tf.placeholder(tf.float32, shape=[None, seq_len, n_features])
 
     hidden_size = 15
-    cell = tf.nn.rnn_cell.BasicRNNCell(num_units=hidden_size)
-    outputs, _states = tf.nn.dynamic_rnn(cell, ph_x, dtype=tf.float32)
+    # cell = tf.nn.rnn_cell.BasicRNNCell(num_units=hidden_size)
+    cells = [tf.nn.rnn_cell.BasicRNNCell(num_units=hidden_size) for _ in range(2)]
+    multi = tf.nn.rnn_cell.MultiRNNCell(cells)
+    outputs, _states = tf.nn.dynamic_rnn(multi, ph_x, dtype=tf.float32)
     print(outputs.shape) # (?, 7, 15)
     #                           (?, 7, 15) seq_len의 마지막값
     hx = tf.layers.dense(outputs[:, -1, :], units= 1, activation=None) # 종가 1개의 값
@@ -78,7 +121,8 @@ def rnn_stock_1():
     sess.close()
 
 
-rnn_stock_1()
+# rnn_stock(make_data_1())
+rnn_stock(make_data_2())
 
 
 
